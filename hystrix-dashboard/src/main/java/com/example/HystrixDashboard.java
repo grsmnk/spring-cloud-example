86d9eaf859d3aaf6c12484b6921f438598162511
Vladyslav Herasymenko <v.herasymenko@synapse.com>
2019-01-10 15:26:38 +0200

    Initial commit

---
+++ hystrix-dashboard/src/main/java/com/example/HystrixDashboard.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class HystrixDashboard {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard.class, args);
+    }
+}
---
+++ web/src/main/java/com/example/web/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RefreshScope
+@RestController
+@Slf4j
+public class HelloController {
+
+    @Value("${greeting}")
+    private String greeting;
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable String name) throws UnknownHostException {
+        log.info("processing request on " + InetAddress.getLocalHost());
+        return String.format("%s, %s", greeting, name);
+    }
+}
